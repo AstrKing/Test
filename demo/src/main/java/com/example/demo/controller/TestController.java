0027eacf396cdfd413603d4b45b44c28cef6b2e3
wyh <444722316@qq.com>
2023-12-27 23:09:25 +0800

    2023年12月27日23:04:10   演示

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/helloWorld")
     public String helloWorld() {
-        return "helloWorld";
+        return "123";
     }
 }
