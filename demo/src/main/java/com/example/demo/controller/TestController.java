439f8933f67a527ad40cde284c2ce3fba844444a
wyh <444722316@qq.com>
2023-12-27 23:05:55 +0800

    2023年12月27日23:04:10

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author wyh
+ * @Date 2023/12/27
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/helloWorld")
+    public String helloWorld() {
+        return "helloWorld";
+    }
+}
