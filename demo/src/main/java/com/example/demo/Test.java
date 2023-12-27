fbe3e31fc0caa3b072fc6ff504b5c40fbbab3fe8
wangyh <444722316@qq.com>
2023-12-27 22:46:52 +0800

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @Author wyh
+ * @Date 2023/12/27
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
